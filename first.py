c071ee75fd06c200e673ec6bb4e9962343864231
Sudarshan <sudarshansonwane2024@gmail.com>
2025-01-28 16:20:23 +0000

    new file:   abc.sh 	new file:   first.py 	new file:   hello.class 	new file:   hello.java

---
+++ first.py
@@ -0,0 +1,14 @@
+class abc:
+    def __init__(self):
+        self.data = 0
+    
+    def display(self):
+        print("Data is: ", self.data)
+
+
+obj = abc()
+
+obj.data = "sudarshan"
+obj.display()
+
+print("Hello world, Sudarshan Sonawane")
